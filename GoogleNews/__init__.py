c4331fc7861c07ec8151828f65e4cab78082a5ab
Hurin <hurin@hurin-VirtualBox>
2019-01-15 15:12:46 +1300

    add exception handle

--- GoogleNews/__init__.py
+++ GoogleNews/__init__.py
@@ -17,16 +17,19 @@ class GoogleNews():
                 self.user_agent='Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
                 self.headers={'User-Agent':self.user_agent}
                 self.url="https://www.google.com/search?q="+self.key+"&tbm=nws&start=%d" % (10*(page-1)) 
-                self.req=urllib.request.Request(self.url, headers=self.headers)
-                self.response=urllib.request.urlopen(self.req)
-                self.page=self.response.read()
-                self.content=soup(self.page, "html.parser")
-                result=self.content.find(id="ires").find_all("div", class_="g")
-                for item in result:
-                        self.texts.append(item.find("h3").text)
-                        self.links.append(item.find("h3").find("a").get("href"))
-                        self.results.append({'title':item.find("h3").text,'media':item.find("div", class_="slp").find_all("span")[0].text,'date':item.find("div", class_="slp").find_all("span")[2].text,'desc':item.find("div", class_="st").text,'link':item.find("h3").find("a").get("href"),'img':item.find("img").get("src")})
-                self.response.close()
+                try:
+                        self.req=urllib.request.Request(self.url, headers=self.headers)
+                        self.response=urllib.request.urlopen(self.req)
+                        self.page=self.response.read()
+                        self.content=soup(self.page, "html.parser")
+                        result=self.content.find(id="ires").find_all("div", class_="g")
+                        for item in result:
+                                self.texts.append(item.find("h3").text)
+                                self.links.append(item.find("h3").find("a").get("href"))
+                                self.results.append({'title':item.find("h3").text,'media':item.find("div", class_="slp").find_all("span")[0].text,'date':item.find("div", class_="slp").find_all("span")[2].text,'desc':item.find("div", class_="st").text,'link':item.find("h3").find("a").get("href"),'img':item.find("img").get("src")})
+                        self.response.close()
+                except Exception:
+                        pass
 
         def result(self):
                 return self.results
@@ -37,7 +40,7 @@ class GoogleNews():
         def getlink(self):
                 return self.links
 
-        def clear():
+        def clear(self):
                 self.texts = []
                 self.links = []
                 self.results = []
