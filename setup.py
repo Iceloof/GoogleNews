8ed3a923e2459a671b5d31357585d6104a0de665
Hurin Hu <hurin@live.ca>
2020-11-16 23:19:27 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Google News search for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/HurinHu/GoogleNews",
+    url="https://github.com/Iceloof/GoogleNews",
     packages=setuptools.find_packages(),
     install_requires=['beautifulsoup4','dateparser','python-dateutil'],
     classifiers=[
