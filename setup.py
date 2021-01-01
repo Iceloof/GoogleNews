65011d8a4f97711c641ab261cc21f5d67feca34d
hurinhu <hurin@live.ca>
2021-01-01 23:26:00 +1300

    modify data range search and order by date

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GoogleNews",
-    version="1.5.1",
+    version="1.5.2",
     author="Hurin Hu",
     author_email="hurin@live.ca",
     description="Google News search for Python",
