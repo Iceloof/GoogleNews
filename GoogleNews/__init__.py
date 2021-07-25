87aec28be0860c8c734cd82658df173dbd9a1b5e
Hugo <hugo.evers@gmail.com>
2021-07-25 14:12:56 +0000

    adding markets

--- GoogleNews/__init__.py
+++ GoogleNews/__init__.py
@@ -67,7 +67,7 @@ class GoogleNews:
         self.user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
         self.accept_language = accept_language 
         self.headers = {'User-Agent': self.user_agent, "Accept-Language":self.accept_language}
-        self.__lang = lang
+        self.__lang = lang # todo: unify this accept language and country search
         self.__period = period
         self.__start = start
         self.__end = end
