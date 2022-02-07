f50e21cb6f3c09e589ff6b7d92ad614ef43f5ac4
jacobtye <jacob.h.tye@gmail.com>
2022-02-07 10:46:30 -0700

    added versions to setup.py

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setuptools.setup(
         "beautifulsoup4",
         "dateparser",
         "python-dateutil",
-        "selenium",
-        "webdriver-manager",
+        "selenium==4.1.0",
+        "webdriver-manager==3.5.2",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
